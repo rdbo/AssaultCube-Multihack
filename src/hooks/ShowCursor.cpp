b28e47f2a462ae9d332989c1f4ee45b0309c3237
rdbo <rdbodev@gmail.com>
2021-01-14 13:21:10 -0300

    fixed cursor grabbing issue

--- src/hooks/ShowCursor.cpp
+++ src/hooks/ShowCursor.cpp
@@ -1,18 +1,22 @@
 #include <pch.h>
 #include <base.h>
 
+static bool bEnabled = false;
+
 int SDLCALL Base::Hooks::ShowCursor(int toggle)
 {
 	if (Data::ShowMenu)
 	{
 		SDL_WM_GrabInput(SDL_GRAB_OFF);
 		Data::oShowCursor(SDL_ENABLE);
+		bEnabled = true;
 	}
 
-	else
+	else if(bEnabled)
 	{
 		SDL_WM_GrabInput(SDL_GRAB_ON);
 		Data::oShowCursor(SDL_DISABLE);
+		bEnabled = false;
 	}
 
 	return 0;
