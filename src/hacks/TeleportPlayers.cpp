b9225ac7f58d28cedd8bd56b54ded838a6aef4f0
rdbo <rdbodev@gmail.com>
2021-01-14 17:25:33 -0300

    fixed teleported player angles

--- src/hacks/TeleportPlayers.cpp
+++ src/hacks/TeleportPlayers.cpp
@@ -14,6 +14,14 @@ void Base::Hacks::TeleportPlayers(playerent* p_ent)
 			pos.x += cos(yaw * (PI / 180.0f)) * Data::Settings::TeleportPlayersDistance;
 			pos.y += sin(yaw * (PI / 180.0f)) * Data::Settings::TeleportPlayersDistance;
 			p_ent->o = pos;
+
+			//Make teleported player look at localplayer
+
+			float ent_yaw = Data::game.player1->yaw + 180.0f;
+			if (ent_yaw > 360.0f) ent_yaw -= 360.0f;
+
+			p_ent->yaw = ent_yaw;
+			p_ent->pitch = Data::game.player1->pitch;
 		}
 
 	}
